3b76928038866f35f1b72b0a887b4620fc7b093a
linkaixing <867047310@qq.com>
2016-04-09 23:33:40 +0800

    first commit

--- Appraise/Info/cleanData.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS `ocenter_appraise`;
-DROP TABLE IF EXISTS `ocenter_appraise_lesson`;
-DROP TABLE IF EXISTS `ocenter_appraise_session`;
-DROP TABLE IF EXISTS `ocenter_appraise_student_lesson`;
-DROP TABLE IF EXISTS `ocenter_appraise_teacher_lesson`;
